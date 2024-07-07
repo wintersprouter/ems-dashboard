6bfb15f2930b6df657bd2e39568f19c8c27cd57f
ashley <ashleyhsueh33@gmail.com>
2024-07-07 16:05:45 +0800

    feat: Add Root component for main route

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./App";
 import "./index.css";
+import Root from "./routes/root";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <Root />,
   },
 ]);
 
