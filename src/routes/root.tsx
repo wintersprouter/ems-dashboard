5463576132475ca86f5a00b6a9931b074d48e122
ashley <ashleyhsueh33@gmail.com>
2024-07-07 22:01:11 +0800

    refactor: Update root component to use flexbox layout

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,13 +1,13 @@
 import { Outlet } from "react-router-dom";
-import Header from "../components/header";
 import Sidebar from "../components/sidebar";
 
 function Root() {
   return (
     <main className='bg-gray-100 flex flex-row h-screen'>
       <Sidebar />
-      <Header />
-      <Outlet />
+      <section className='flex-grow'>
+        <Outlet />
+      </section>
     </main>
   );
 }
