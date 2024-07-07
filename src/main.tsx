d4b19f9877429cb32588ee4ea5017db9932e2802
ashley <ashleyhsueh33@gmail.com>
2024-07-07 16:08:50 +0800

    feat: Add error page component for handling unexpected errors

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./error-page";
 import "./index.css";
 import Root from "./routes/root";
 
@@ -8,6 +9,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
