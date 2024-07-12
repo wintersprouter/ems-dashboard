0cbee2e4157619e6ba7f5ca9526b52775cdc6b44
ashley <ashleyhsueh33@gmail.com>
2024-07-12 23:15:09 +0800

    refactor: Remove unused SVG files and update header component styling

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,13 +2,28 @@ function Header() {
   return (
     <div
       id='header'
-      className='flex h-20 justify-between align-middle px-4 py-2'
+      className='flex  justify-between px-4 py-2 bg-white border-l rounded-full items-center'
     >
-      <h1 className='logo text-3xl text-green-600 font-semibold'>Epower OS</h1>
-      <div></div>
+      <div className='flex gap-8'>
+        <h1 className='logo text-3xl text-green-600 font-semibold'>
+          Epower OS
+        </h1>
+        <div className='rounded-full bg-green-50 flex'>
+          <div className='rounded-[32px] py-2 px-6'>
+            <p className='text-black font-normal text-base'>Home</p>
+          </div>
+          <div className='rounded-[32px] py-2 px-6 bg-green-600'>
+            <p className='text-white font-normal text-base'>Data</p>
+          </div>
+        </div>
+      </div>
       <div className='flex justify-items-center'>
-        {/* <img className='rounded-full bg-slate-600' alt='avatar'></img> */}
-        <p className='text-black text font-normal'>username</p>
+        <img
+          className='rounded-full bg-slate-600'
+          alt='avatar'
+          src='../../public/username.png'
+        ></img>
+        <p className='text-black text-base font-normal'>username</p>
       </div>
     </div>
   );
