687be811f91b7601dab0a635461b5605d4b72a08
ashley <ashleyhsueh33@gmail.com>
2024-07-21 17:17:35 +0800

    chore: Update toolbar component to include date range inputs

--- src/components/toolbar.tsx
+++ src/components/toolbar.tsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
+
 const ToolBar = () => {
+  const [startTime, setStartTime] = useState("");
+  const [endTime, setEndTime] = useState("");
+
+  const handleStartTimeChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const newStartTime: string = event.target.value;
+    setStartTime(newStartTime);
+    if (newStartTime > endTime) {
+      setEndTime(newStartTime);
+    }
+  };
+
+  const handleEndTimeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEndTime(event.target.value);
+  };
+
   return (
     <section
-      className='relative top-36 lg:top-4 lg:left-48 bg-white rounded-2xl gap-4 p-4 pl-8 flex flex-col divide-y-[1px] divide-gray-300  max-w-52 h-[42rem]
+      className='relative top-36 lg:top-4 lg:left-48 bg-white rounded-2xl gap-4 p-4 pl-8 flex flex-col divide-y-[1px] divide-gray-300  max-w-72 h-[42rem]
     '
     >
       <div className='grid grid-cols-2 gap-1'>
@@ -56,6 +75,19 @@ const ToolBar = () => {
       </div>
       <div>
         <h4 className='my-4 font-medium text-gray-800'>Date Range</h4>
+        <input
+          aria-label='Date and time from'
+          value={startTime}
+          onChange={handleStartTimeChange}
+          type='datetime-local'
+        />
+        <input
+          aria-label='Date and time to'
+          min={startTime}
+          value={endTime}
+          onChange={handleEndTimeChange}
+          type='datetime-local'
+        />
       </div>
       <div>
         <h4 className='my-4 font-medium text-gray-800'>Introduction</h4>
