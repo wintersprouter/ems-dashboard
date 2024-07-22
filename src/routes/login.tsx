2ee0d1a21f6669eb20f4fc9b5427715d924bb752
ashley <ashleyhsueh33@gmail.com>
2024-07-22 23:09:39 +0800

    chore: Update login page layout and styles

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -48,69 +48,57 @@ function Login() {
 
   return (
     <FormProvider {...methods}>
-      <main className='w-full h-screen flex flex-col items-center justify-center px-4'>
-        <div className='max-w-sm w-full text-gray-600'>
-          <div className='text-center'>
-            <div className=' flex justify-center flex-col'>
-              <h1 className='text-4xl font-bold'>EMS</h1>
-              <h2>智慧電能管理系統</h2>
-            </div>
-          </div>
-          <form className='mt-8 space-y-5' onSubmit={handleSubmit(onSubmit)}>
-            <div>
-              <label className='font-medium' htmlFor='email'>
-                Email
-                <input
-                  id='email'
-                  type='email'
-                  required
-                  autoComplete='email'
-                  className='w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border  shadow-sm rounded-lg'
-                  {...register("email")}
-                  placeholder='Example@email.com'
-                  autoFocus
-                />
-              </label>
-            </div>
-            <div>
-              <label className='font-medium' htmlFor='password'>
-                Password
-                <input
-                  id='password'
-                  type='password'
-                  required
-                  autoComplete='current-password'
-                  className='w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border  shadow-sm rounded-lg'
-                  placeholder='at least 8 characters'
-                  {...register("password")}
-                />
-              </label>
-            </div>
-            <ErrorMessage
-              errors={errors}
-              name='password'
-              render={({ message }) => (
-                <p className='text-red-600'>{message}</p>
-              )}
+      <main className='w-full h-screen flex justify-center items-start md:items-center'>
+        <form
+          className='space-y-5 max-w-sm w-full text-gray-600
+          bg-white p-5 rounded-lg shadow-lg'
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <label className='font-medium' htmlFor='email'>
+            Email
+            <input
+              id='email'
+              type='email'
+              required
+              autoComplete='email'
+              className='w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border  shadow-sm rounded-lg'
+              {...register("email")}
+              placeholder='Example@email.com'
+              autoFocus
             />
-            <ErrorMessage
-              errors={errors}
-              name='email'
-              render={({ message }) => (
-                <p className='text-red-600'>{message}</p>
-              )}
+          </label>
+
+          <label className='font-medium' htmlFor='password'>
+            Password
+            <input
+              id='password'
+              type='password'
+              required
+              autoComplete='current-password'
+              className='w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border  shadow-sm rounded-lg'
+              placeholder='at least 8 characters'
+              {...register("password")}
             />
-            <button
-              type='submit'
-              disabled={isLoading}
-              className='w-full  py-4 text-white font-medium bg-gray-800 rounded-full duration-150'
-            >
-              <p className='text-xl'>
-                {isLoading ? "Logging in..." : "Sign in"}
-              </p>
-            </button>
-          </form>
-        </div>
+          </label>
+
+          <ErrorMessage
+            errors={errors}
+            name='password'
+            render={({ message }) => <p className='text-red-600'>{message}</p>}
+          />
+          <ErrorMessage
+            errors={errors}
+            name='email'
+            render={({ message }) => <p className='text-red-600'>{message}</p>}
+          />
+          <button
+            type='submit'
+            disabled={isLoading}
+            className='w-full  py-4 text-white font-medium bg-gray-800 rounded-full duration-150'
+          >
+            <p className='text-xl'>{isLoading ? "Logging in..." : "Sign in"}</p>
+          </button>
+        </form>
       </main>
     </FormProvider>
   );
