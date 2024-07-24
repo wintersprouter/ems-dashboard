2369f9940d9be3c5fecba0163c3d469600f862aa
ashley <ashleyhsueh33@gmail.com>
2024-07-24 15:07:21 +0800

    chore: Update login page to use AuthProvider for sign-in functionality and improve form layout and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import {
   createBrowserRouter,
+  LoaderFunction,
+  LoaderFunctionArgs,
   Navigate,
+  redirect,
   RouterProvider,
 } from "react-router-dom";
 import "./App.css";
+import { AuthProvider } from "./context/authProvider";
 import ErrorPage from "./error-page";
 import Home from "./routes/home";
 import Login from "./routes/login";
@@ -11,6 +15,48 @@ import MonitoringPoint from "./routes/monitoring-point";
 import Overview from "./routes/overview";
 import Root from "./routes/root";
 import Tree from "./routes/tree";
+
+export function Fallback() {
+  return <p>Performing initial data load</p>;
+}
+
+async function loginLoader() {
+  AuthProvider.getAuthStatus();
+  if (AuthProvider.isAuthenticated) {
+    return redirect("/");
+  }
+  return null;
+}
+
+async function requireAuth() {
+  await AuthProvider.getAuthStatus();
+  if (!AuthProvider.isAuthenticated) {
+    return redirect("/dashboard/login");
+  }
+  return null;
+}
+
+function ProtectedRoute({
+  element,
+  loader,
+}: {
+  element: JSX.Element;
+  loader?: LoaderFunction;
+}) {
+  return {
+    element,
+    loader: async (args: LoaderFunctionArgs) => {
+      const redirectResult = requireAuth();
+      if (redirectResult) {
+        return redirectResult;
+      }
+      if (loader) {
+        return await loader(args);
+      }
+      return null;
+    },
+  };
+}
 
 function App() {
   const router = createBrowserRouter([
@@ -32,23 +78,24 @@ function App() {
       children: [
         {
           path: "",
-          element: <Navigate to='overview' />,
+          ...ProtectedRoute({ element: <Navigate to='overview' /> }),
         },
         {
           path: "login",
           element: <Login />,
+          loader: loginLoader,
           index: true,
         },
         {
-          element: <Overview />,
+          ...ProtectedRoute({ element: <Overview /> }),
           path: "overview",
         },
         {
-          element: <Tree />,
+          ...ProtectedRoute({ element: <Tree /> }),
           path: "tree",
         },
         {
-          element: <MonitoringPoint />,
+          ...ProtectedRoute({ element: <MonitoringPoint /> }),
           path: "monitoring-point",
         },
       ],
