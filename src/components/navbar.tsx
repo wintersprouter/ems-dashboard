514c1166055bc27fa7caf39d9e9d4fee21a6f378
ashley <ashleyhsueh33@gmail.com>
2024-08-02 22:57:53 +0800

    feat: Update navbar and sidebar components
    This commit updates the navbar and sidebar components to improve the navigation experience. The navbar component now includes icons and active states for each navigation item, enhancing the visual feedback for users. The sidebar component has been refactored to use the updated navbar component, ensuring consistency across the application. These changes enhance the usability and aesthetics of the navigation elements.

---
+++ src/components/navbar.tsx
@@ -0,0 +1,75 @@
+import { NavLink } from "react-router-dom";
+import monitoring_point_active_icon from "../assets/monitoring_point_active_icon.svg";
+import monitoring_point_icon from "../assets/monitoring_point_icon.svg";
+import overview_active_icon from "../assets/overview_active_icon.svg";
+import overview_icon from "../assets/overview_icon.svg";
+import tree_active_icon from "../assets/tree_active_icon.svg";
+import tree_icon from "../assets/tree_icon.svg";
+const navLinkStyle =
+  "flex px-1 pl-2 hover:bg-green-100 hover:rounded duration-150 ease-in-out";
+const activeNavLinkStyle =
+  "flex px-1  rounded bg-green-100 border-green-400 border duration-150 ease-in-out";
+
+const navTextStyle =
+  "p-2 text-base text-gray-800 font-normal hover:text-green-800 hidden lg:flex ";
+const navActiveTextStyle =
+  "p-2 text-base text-green-800 font-normal hidden lg:flex ";
+
+const navigation = [
+  {
+    navigation: "/dashboard/overview",
+    name: "Overview",
+    icon: overview_icon,
+    activeIcon: overview_active_icon,
+  },
+
+  {
+    navigation: "/dashboard/tree",
+    name: "Tree",
+    icon: tree_icon,
+    activeIcon: tree_active_icon,
+  },
+  {
+    navigation: "/dashboard/monitoring-point?deviceId=0",
+    name: "Monitoring Point",
+    icon: monitoring_point_icon,
+    activeIcon: monitoring_point_active_icon,
+  },
+];
+const Navbar = () => {
+    return(
+        <nav className='pb-4'>
+        <h2 className='text-left text-xs text-gray-400'>General</h2>
+        <ul className='flex flex-col gap-3 my-3'>
+          {navigation.map((item, idx) => (
+            <NavLink key={`NavLink-${idx}`} to={item.navigation}>
+              {({ isActive, isPending }) => (
+                <li
+                  className={
+                    isActive
+                      ? activeNavLinkStyle
+                      : isPending
+                      ? navLinkStyle
+                      : navLinkStyle
+                  }
+                >
+                  {isActive && (
+                    <div className='relative right-4 top-0 h-10 w-1 bg-green-800' />
+                  )}
+                  <img
+                    src={isActive ? item.activeIcon : item.icon}
+                    alt={item.navigation}
+                  />
+                  <p className={isActive ? navActiveTextStyle : navTextStyle}>
+                    {item.name}
+                  </p>
+                </li>
+              )}
+            </NavLink>
+          ))}
+        </ul>
+      </nav>
+    )
+
+}
+export default Navbar
--- src/components/toolbar.tsx
+++ src/components/toolbar.tsx
@@ -1,51 +1,18 @@
 import { format } from "date-fns/format";
 import { useEffect } from "react";
-import { NavLink, useNavigate, useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { z } from "zod";
-import monitoring_point_active_icon from "../assets/monitoring_point_active_icon.svg";
-import monitoring_point_icon from "../assets/monitoring_point_icon.svg";
-import overview_active_icon from "../assets/overview_active_icon.svg";
-import overview_icon from "../assets/overview_icon.svg";
-import tree_active_icon from "../assets/tree_active_icon.svg";
-import tree_icon from "../assets/tree_icon.svg";
 import { DeviceUsageInfo } from "../routes/monitoring-point";
 import { monitorDeviceResponse } from "../services/apis/web";
-const navLinkStyle =
-  "flex px-1 pl-2 hover:bg-green-100 hover:rounded duration-150 ease-in-out";
-const activeNavLinkStyle =
-  "flex px-1  rounded bg-green-100 border-green-400 border duration-150 ease-in-out";
+import Navbar from "./navbar";
 
-const navTextStyle =
-  "p-2 text-base text-gray-800 font-normal hover:text-green-800 hidden lg:flex ";
-const navActiveTextStyle =
-  "p-2 text-base text-green-800 font-normal hidden lg:flex ";
 
 const defaultDateStyle =
   "relative left-0 rounded-lg border border-gray-300 p-2 text-sm text-gray-300 font-normal active:border-gray-800 active:text-gray-800";
 const hasDateStyle =
   "relative left-0 rounded-lg border p-2 text-sm font-normal border-gray-800 text-gray-800";
 
-const navigation = [
-  {
-    navigation: "/dashboard/overview",
-    name: "Overview",
-    icon: overview_icon,
-    activeIcon: overview_active_icon,
-  },
 
-  {
-    navigation: "/dashboard/tree",
-    name: "Tree",
-    icon: tree_icon,
-    activeIcon: tree_active_icon,
-  },
-  {
-    navigation: "/dashboard/monitoring-point?deviceId=0",
-    name: "Monitoring Point",
-    icon: monitoring_point_icon,
-    activeIcon: monitoring_point_active_icon,
-  },
-];
 
 type ToolBarProps = {
   deviceList: z.infer<typeof monitorDeviceResponse>["deviceList"];
@@ -99,39 +66,7 @@ const ToolBar = ({
   return (
     <section className='fixed top-4 lg:top-20 lg:left-2 bg-white rounded-2xl  flex h-[48rem]'>
       <div className='rounded-2xl border border-white shadow min-w-28 py-6 px-3 overflow-y-auto bg-gray-50 grid grid-cols-1 divide-y-2 divide-gray-200 gap-6 h-full '>
-        <nav className='pb-4'>
-          <h2 className='text-left text-xs text-gray-400'>General</h2>
-          <ul className='flex flex-col gap-3 my-3'>
-            {navigation.map((item, idx) => (
-              <NavLink to={item.navigation} key={`navigation-${idx}`}
-              >
-                {({ isActive, isPending }) => (
-                  <li
-                    key={idx}
-                    className={
-                      isActive
-                        ? activeNavLinkStyle
-                        : isPending
-                        ? navLinkStyle
-                        : navLinkStyle
-                    }
-                  >
-                    {isActive && (
-                      <div className='relative right-4 top-0 h-10 w-1 bg-green-800' />
-                    )}
-                    <img
-                      src={isActive ? item.activeIcon : item.icon}
-                      alt={item.navigation}
-                    />
-                    <p className={isActive ? navActiveTextStyle : navTextStyle}>
-                      {item.name}
-                    </p>
-                  </li>
-                )}
-              </NavLink>
-            ))}
-          </ul>
-        </nav>
+        <Navbar/>
       </div>
       <div className='flex flex-col divide-y-[1px] divide-gray-300 p-4 gap-4 w-64'>
         <div className='grid grid-cols-2 gap-1'>
