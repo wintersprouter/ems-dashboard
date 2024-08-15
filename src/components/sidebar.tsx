083af7a39f5109b5418c440f7ee94eec53f7eff4
ashley <ashleyhsueh33@gmail.com>
2024-08-15 21:40:23 +0800

    feat: Update sidebar component to adjust height dynamically

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -5,7 +5,7 @@ function Sidebar() {
     <aside
       id='default-sidebar'
       className='
-      fixed top-36 lg:top-20 left-1 z-40 h-full rounded-2xl border border-white mx-2 shadow min-w-28 py-6 px-3 overflow-y-auto bg-gray-50 hidden lg:flex'
+      fixed top-36 lg:top-20 left-1 z-40 h-[calc(100vh-6rem)] rounded-2xl border border-white mx-2 shadow min-w-28 py-6 px-3 overflow-y-auto bg-gray-50 hidden lg:flex'
       aria-label='Sidebar'
     >
       <div className='grid grid-cols-1 divide-y-2 divide-gray-200 gap-6 h-full '>
