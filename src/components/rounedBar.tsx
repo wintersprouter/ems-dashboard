57db9066285b391626b93737742c3529942feb41
ashley <ashleyhsueh33@gmail.com>
2024-08-15 23:11:32 +0800

    fix: Fix RoundedBar component calculations for xWithWidth and yWithHeight

--- src/components/rounedBar.tsx
+++ src/components/rounedBar.tsx
@@ -3,14 +3,15 @@ import { BarProps } from "recharts";
 const RoundedBar = (props: BarProps) => {
   const { x, y, width, height, fill } = props;
   const radius = 6;
-  const xWithWidth = x + width;
-  const yWithHeight = y + height;
-  const yWithRadius = y + radius;
+  const xWithWidth = x && width ? Number(x) + width : 0;
+  const yWithHeight = height ? Number(y) + height : 0;
+  const yWithRadius = Number(y) + radius;
+  const xWithRadius = Number(x) + radius;
   return (
     <path
       d={`M${x},${yWithHeight} 
          L${x},${yWithRadius} 
-         Q${x},${y} ${x + radius},${y} 
+         Q${x},${y} ${xWithRadius},${y} 
          L${xWithWidth - radius},${y} 
          Q${xWithWidth},${y} ${xWithWidth},${yWithRadius} 
          L${xWithWidth},${yWithHeight} 
