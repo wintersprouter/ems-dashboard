1ffb2f9167621c3eb28fb82b0ff5f8e3f13993e9
ashley <ashleyhsueh33@gmail.com>
2024-08-17 23:56:55 +0800

    :bug: fix get monitorDevice info

--- src/routes/monitoring-point.tsx
+++ src/routes/monitoring-point.tsx
@@ -1,4 +1,4 @@
-import { useMutation } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { match } from "ts-pattern";
@@ -23,12 +23,12 @@ function MonitoringPoint() {
   >([]);
   const [dtStart, setDtStart] = useState(startDate ?? "");
   const [dtEnd, setDtEnd] = useState(endDate ?? "");
-  const [device, setDevice] = useState<DeviceUsageInfo>();
 
   const navigate = useNavigate();
-  const { status, mutate, failureReason } = useMutation({
-    mutationFn: () => {
-      return Api.monitorDevice({
+  const { data, status, failureReason } = useQuery({
+    queryKey: ["monitorDevice", deviceId, dtStart, dtEnd],
+    queryFn: () =>
+      Api.monitorDevice({
         email: AuthProvider.email ?? "",
         action: "monitorDevice",
         token: AuthProvider.token ?? "",
@@ -36,36 +36,18 @@ function MonitoringPoint() {
         deviceId: deviceId,
         dtStart: dtStart,
         dtEnd: dtEnd,
-      });
-    },
-    onSuccess(data) {
-      // console.log("data", JSON.stringify(data, null, 2));
+      }),
+    enabled: !!AuthProvider.email && !!AuthProvider.token,
+  });
+
+  useEffect(() => {
+    if (status === "success") {
       setDtStart(data.deviceUsageInfo?.dtStart ?? "");
       setDtEnd(data.deviceUsageInfo?.dtEnd ?? "");
-      setDevice({
-        ...device,
-        ...data.deviceUsageInfo,
-        realtimeSmartMeterInfo: {
-          ...device?.realtimeSmartMeterInfo,
-          ...data.deviceUsageInfo.realtimeSmartMeterInfo,
-          usedChannel: [
-            ...data.deviceUsageInfo.realtimeSmartMeterInfo.usedChannel,
-          ],
-        },
-        averagePowerUsage: {
-          ...device?.averagePowerUsage,
-          ...data.deviceUsageInfo.averagePowerUsage,
-        },
-        deviceUsage: {
-          ...device?.deviceUsage,
-          ...data.deviceUsageInfo.deviceUsage,
-        },
-      });
       setDeviceList(
         deviceList.length === 0
           ? [...data.deviceList]
           : [
-              //保留舊資料中新資料也有的
               ...deviceList.filter((device) => {
                 const newDevice = data.deviceList.find(
                   (d) => d.id === device.id
@@ -77,19 +59,24 @@ function MonitoringPoint() {
                   };
                 }
               }),
-              //加入新資料中有舊資料沒有的
               ...data.deviceList.filter(
                 (newDevice) => !deviceList.some((d) => d.id === newDevice.id)
               ),
             ]
       );
-      return navigate(`/dashboard/monitoring-point?${searchParams.toString()}`);
-    },
-  });
-
-  useEffect(() => {
-    mutate();
-  }, [mutate]);
+    }
+    if (searchParams) {
+      navigate(`/dashboard/monitoring-point?${searchParams.toString()}`);
+    }
+    // data.deviceList,
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [
+    data?.deviceUsageInfo?.dtEnd,
+    data?.deviceUsageInfo?.dtStart,
+    navigate,
+    searchParams,
+    status,
+  ]);
 
   return (
     <section className='flex'>
@@ -106,17 +93,19 @@ function MonitoringPoint() {
               setDtEnd={setDtEnd ?? ""}
               dtStart={dtStart}
               dtEnd={dtEnd}
-              device={device}
+              device={data?.deviceUsageInfo}
             />
             <section className='relative top-0 left-0 flex-col w-full mx-auto lg:left-[21.5rem] lg:top-2 lg:max-w-[calc(100%-17rem)] lg:w-4/5  xl:left-[14.5rem] xl:w-[calc(100%-30rem)]'>
               <StatisticsCards
-                totalUsageKW={device?.totalUsageKW ?? 0}
-                realtimeSmartMeterInfo={device?.realtimeSmartMeterInfo}
-                averagePowerUsage={device?.averagePowerUsage}
+                totalUsageKW={data?.deviceUsageInfo.totalUsageKW ?? 0}
+                realtimeSmartMeterInfo={
+                  data?.deviceUsageInfo.realtimeSmartMeterInfo
+                }
+                averagePowerUsage={data?.deviceUsageInfo.averagePowerUsage}
               />
               <Chart
                 deviceUsage={
-                  device?.deviceUsage ?? {
+                  data?.deviceUsageInfo.deviceUsage ?? {
                     usage: [],
                     id: 0,
                     name: "",
@@ -131,7 +120,6 @@ function MonitoringPoint() {
             </section>
           </>
         ))
-        .with("idle", () => <p>Idle</p>)
         .exhaustive()}
     </section>
   );
